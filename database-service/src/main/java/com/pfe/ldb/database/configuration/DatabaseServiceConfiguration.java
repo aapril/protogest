2d89d1ab6459a4ad5202165a776e6b03cbb7f45e
Hugo Lapointe Di Giacomo <hugo.lapo@gmail.com>
2018-10-18 22:38:31 -0400

    Petites corrections
    Petites corrections suites à quelques tests d'intégration avec les autres services.

--- database-service/src/main/java/com/pfe/ldb/database/configuration/DatabaseServiceConfiguration.java
+++ database-service/src/main/java/com/pfe/ldb/database/configuration/DatabaseServiceConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@EntityScan("com.pfe.ldb.entity")
+@EntityScan("com.pfe.ldb.entities")
 @Profile("dev")
 public class DatabaseServiceConfiguration {
 
