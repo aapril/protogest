ce81fdb704d3aff5effb2102a44eea93ac9816d9
Emile Robinson <emile.c.robinson@gmail.com>
2018-11-22 21:50:46 -0500

    test task-service

--- task-service/src/main/java/com/pfe/ldb/task/TaskServiceApplication.java
+++ task-service/src/main/java/com/pfe/ldb/task/TaskServiceApplication.java
@@ -2,12 +2,13 @@ package com.pfe.ldb.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 @EnableEurekaClient
-public class TaskServiceApplication {
+public class TaskServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskServiceApplication.class, args);
