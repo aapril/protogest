f66084c8c2c4e66e587946f026c28d78259831a8
Emile Robinson <emile.c.robinson@gmail.com>
2018-11-28 21:45:14 -0500

    test member-service

--- member-service/src/main/java/com/pfe/ldb/member/MemberServiceApp.java
+++ member-service/src/main/java/com/pfe/ldb/member/MemberServiceApp.java
@@ -2,12 +2,12 @@ package com.pfe.ldb.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 
 @SpringBootApplication
 @EnableEurekaClient
-public class MemberServiceApp {
+public class MemberServiceApp extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MemberServiceApp.class, args);
