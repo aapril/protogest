be6912bf21e4844fc23117e8f5fe89d8d7cfde33
Hugo Lapointe Di Giacomo <hugo.lapo@gmail.com>
2018-11-29 11:44:04 -0500

    Small amend.

--- member-service/src/main/java/com/pfe/ldb/member/dto/MemberUpdateDTO.java
+++ member-service/src/main/java/com/pfe/ldb/member/dto/MemberUpdateDTO.java
@@ -11,17 +11,14 @@ import lombok.NoArgsConstructor;
 public class MemberUpdateDTO {
 
 	@ApiModelProperty(position = 0, required = true)
-	private Integer id;
-
-	@ApiModelProperty(position = 1, required = true)
 	private @NotNull String firstName;
 
-	@ApiModelProperty(position = 2, required = true)
+	@ApiModelProperty(position = 1, required = true)
 	private @NotNull String lastName;
 
-	@ApiModelProperty(position = 3, required = true)
+	@ApiModelProperty(position = 2, required = true)
 	private @NotNull String email;
 
-	@ApiModelProperty(position = 4, required = true)
+	@ApiModelProperty(position = 3, required = true)
 	private @NotNull Integer userId;
 }
