652f0af648784d74b61d657bdb4d3ee36e40964f
Emile Robinson <emile.c.robinson@gmail.com>
2018-12-02 22:26:15 -0500

    ajustement 2 member-service

--- member-service/src/main/java/com/pfe/ldb/member/controller/MemberController.java
+++ member-service/src/main/java/com/pfe/ldb/member/controller/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.pfe.ldb.member.dao.exception.MemberEntityNotFoundException;
 import com.pfe.ldb.member.dao.exception.UserEntityNotFoundException;
@@ -24,6 +25,7 @@ import com.pfe.ldb.member.service.MemberService;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
+@EnableWebMvc
 public class MemberController {
 
 	private @Autowired MemberService memberService;
