d74402ae96f1d2192d2e982dfaa4cad9eed7c392
Emile Robinson <emile.c.robinson@gmail.com>
2018-12-08 15:03:38 -0500

    conversion de auth,member,task et event pour beanstalk

--- task-service/src/main/java/com/pfe/ldb/task/TaskServiceApp.java
+++ task-service/src/main/java/com/pfe/ldb/task/TaskServiceApp.java
@@ -2,11 +2,8 @@ package com.pfe.ldb.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 
 @SpringBootApplication
-@EnableEurekaClient
 public class TaskServiceApp {
 
 	public static void main(String[] args) {
