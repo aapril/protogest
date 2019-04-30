9439dd24b94fd33de1b2c287fc9457abcb6ca5f7
= <=>
2019-04-30 19:32:22 -0400

    import static

--- ProtogestService/src/main/java/com/protogest/service/ProtogestApplication.java
+++ ProtogestService/src/main/java/com/protogest/service/ProtogestApplication.java
@@ -3,9 +3,35 @@ package com.protogest.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication(scanBasePackages={
-        "com.protogest.repository", "com.protogest.service"})
+//@SpringBootApplication(scanBasePackages={
+//        "com.protogest.repository", "com.protogest.service"})
+@Configuration
+@Import({AopAutoConfiguration.class,
+    CodecsAutoConfiguration.class,
+    DataSourceAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class,
+    DispatcherServletAutoConfiguration.class,
+    EmbeddedWebServerFactoryCustomizerAutoConfiguration.class,
+    ErrorMvcAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class,
+    HttpEncodingAutoConfiguration.class,
+    HttpMessageConvertersAutoConfiguration.class,
+    JacksonAutoConfiguration.class,
+    JdbcTemplateAutoConfiguration.class,
+    JmxAutoConfiguration.class,
+    JtaAutoConfiguration.class,
+    PersistenceExceptionTranslationAutoConfiguration.class,
+    PropertyPlaceholderAutoConfiguration.class,
+    RestTemplateAutoConfiguration.class,
+    ServletWebServerFactoryAutoConfiguration.class,
+    SpringDataWebAutoConfiguration.class,
+    TransactionAutoConfiguration.class,
+    ValidationAutoConfiguration.class,
+    WebMvcAutoConfiguration.class,
+    WebSocketServletAutoConfiguration.class})
 public class ProtogestApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ProtogestApplication.class, args);
