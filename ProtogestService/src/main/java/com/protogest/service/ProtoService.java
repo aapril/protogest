d6849f58de23f79b6be6edd067128346593cb8a6
Physiix <yanis.m.1992@gmail.com>
2019-06-06 14:34:22 -0400

    Update ProtoService to handle new ProtocolInstance: requests to DynamoDB instead of Mysql.

--- ProtogestService/src/main/java/com/protogest/service/ProtoService.java
+++ ProtogestService/src/main/java/com/protogest/service/ProtoService.java
@@ -1,79 +1,58 @@
 package com.protogest.service;
 
-import com.protogest.model.form.*;
-import com.protogest.model.form.nosql.ProtocoleInstance;
-import com.protogest.repository.ProtocoleInstanceRepository;
-import com.protogest.repository.UserFormRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.protogest.service.database.models.ProtocolInstance;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @Service
 @Transactional
 public class ProtoService {
+    private final DynamoDBMapper mapper;
 
-    private @Autowired
-    UserFormRepository repository;
-
-    public String create(ProtocoleInstance proto, String userId, String relatedUserId) {
-
-        String formUUID = ProtocoleInstanceRepository.save(proto).getFormUUID();
-
-        UserForm userForm = new UserForm();
-        userForm.setFormUUID(formUUID);
-        userForm.setUserId(userId);
-        userForm.setStatus("PENDING");
-        userForm.setRelatedUserId(relatedUserId);
-
-        List<ValidationField> validationFields = buildValidationFields(proto, userForm, relatedUserId);
-        userForm.setValidationFields(validationFields);
-
-        repository.save(userForm);
-
-        proto.setUserID(userForm.getUserId());
-        proto.setFormUUID(formUUID);
-
-        return formUUID;
+    public ProtoService(DynamoDBMapper mapper) {
+        this.mapper = mapper;
     }
 
-    public ProtocoleInstance getByUUID(String UUID) {
-        return ProtocoleInstanceRepository.get(UUID);
+    public String create(ProtocolInstance protocolInstance, String userEmail, String invitedUserEmail) {
+        protocolInstance.setUserEmail(userEmail);
+        protocolInstance.setInvitedUserEmail(invitedUserEmail);
+        protocolInstance.setStatus(ProtocolInstance.Status.PENDING);
+
+        mapper.save(protocolInstance);
+
+        return protocolInstance.getUuid();
     }
 
-    private List<ValidationField> buildValidationFields(ProtocoleInstance proto, UserForm userForm, String relatedUserId) {
-
-        List<ValidationField> fieldsToValidate = proto.getFields()
-                .stream()
-                .filter(p -> p.getType() == EFieldType.DATE)
-                .map(f -> ValidationField.from(f))
-                .collect(Collectors.toList());
-
-        fieldsToValidate.forEach(f -> {
-//            f.setFormUUID(proto.getFormUUID());
-            f.setProposedById(userForm.getUserId());
-            f.setUserForm(userForm);
-            f.addAppobation(new FieldApprobation(proto.getFormUUID(), f, EValidationStatus.PENDING, relatedUserId));
-        });
-
-        return fieldsToValidate;
+    public ProtocolInstance getByUUID(String UUID) {
+        return this.mapper.load(ProtocolInstance.class, UUID);
     }
 
-    public List<ProtocoleInstance> list(String userMail) {
-        return ProtocoleInstanceRepository.getByUserId(userMail);
+
+    public List<ProtocolInstance> list(String userMail) {
+        return getProtocolsByAttribute("userEmail", userMail);
     }
 
-    public List<String> getRelatedFormIds(String userMail) {
-        return repository.getRelatedFormIds(userMail);
+    public List<ProtocolInstance> getInvitedProtocolInstances(String userMail) {
+        return getProtocolsByAttribute("invitedUserEmail", userMail);
     }
 
-    public void update(String formUUID, ProtocoleInstance protocol) {
-        ProtocoleInstance original = ProtocoleInstanceRepository.get(formUUID);
-        if(original != null) {
-            protocol.setFormUUID(formUUID);
-            ProtocoleInstanceRepository.update(protocol);
-        }
+    private List<ProtocolInstance> getProtocolsByAttribute(String attributeName, String attributeValue) {
+        final Map<String, AttributeValue> values = new HashMap<>();
+        values.put(String.format(":%s", attributeName), new AttributeValue().withS(attributeValue));
+        return this.mapper.scan(ProtocolInstance.class, new DynamoDBScanExpression()
+                .withFilterExpression(String.format("%s = :%s", attributeName, attributeName))
+                .withExpressionAttributeValues(values));
+    }
+
+
+    public void update(String formUUID, ProtocolInstance protocol) {
+        // wasn't working
     }
 }
