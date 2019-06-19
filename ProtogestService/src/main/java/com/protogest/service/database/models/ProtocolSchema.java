d89496a0324cc58d9f9b936237f3bf0d75c8b486
Physiix <yanis.m.1992@gmail.com>
2019-06-19 12:04:24 -0400

    ProtocolSchema class matches the Json representation

--- ProtogestService/src/main/java/com/protogest/service/database/models/ProtocolSchema.java
+++ ProtogestService/src/main/java/com/protogest/service/database/models/ProtocolSchema.java
@@ -10,4 +10,119 @@ public class ProtocolSchema {
 
     @DynamoDBAttribute
     private Long id;
+
+    @DynamoDBAttribute(attributeName = "protocol_fields")
+    private ProtocolFields[] protocolFields;
+
+    @DynamoDBAttribute
+    private String description;
+
+    @DynamoDBDocument
+    public class DateSection {
+        private String num;
+
+        private String type;
+
+        private String desc;
+
+        public String getNum() {
+            return num;
+        }
+
+        public void setNum(String num) {
+            this.num = num;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+    }
+
+    @DynamoDBDocument
+    public class ProtocolFields {
+        private String sectionName;
+
+        private SubSection[] subSection;
+
+        private String sectionID;
+
+        public String getSectionName() {
+            return sectionName;
+        }
+
+        public void setSectionName(String sectionName) {
+            this.sectionName = sectionName;
+        }
+
+        public SubSection[] getSubSection() {
+            return subSection;
+        }
+
+        public void setSubSection(SubSection[] subSection) {
+            this.subSection = subSection;
+        }
+
+        public String getSectionID() {
+            return sectionID;
+        }
+
+        public void setSectionID(String sectionID) {
+            this.sectionID = sectionID;
+        }
+    }
+
+    @DynamoDBDocument
+    public class SubSection {
+        private String num;
+
+        private String type;
+
+        private DateSection[] dateSection;
+
+        private String desc;
+
+        public String getNum() {
+            return num;
+        }
+
+        public void setNum(String num) {
+            this.num = num;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public DateSection[] getDateSection() {
+            return dateSection;
+        }
+
+        public void setDateSection(DateSection[] dateSection) {
+            this.dateSection = dateSection;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+    }
 }
