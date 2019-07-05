28e1576792b576902aca4a374940692355393bd5
jeanmichellafranceets <jeanmichellafrance@gmail.ca>
2019-07-05 12:13:18 -0400

    nouvelle fonction pour récupérer amazon compte information

--- ProtogestService/src/main/java/com/protogest/service/users/CognitoService.java
+++ ProtogestService/src/main/java/com/protogest/service/users/CognitoService.java
@@ -4,6 +4,8 @@ import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.model.*;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -87,5 +89,87 @@ public class CognitoService {
         } catch (AWSCognitoIdentityProviderException e) {
             return new CognitoRequestResult(false, e.getErrorMessage(), e.getErrorCode());
         }
+    }
+
+
+    public String getUserFirstName(String userId) {
+
+
+        AdminGetUserRequest  userRequest  = new AdminGetUserRequest().withUserPoolId("ca-central-1_ZBpO9AVc6").withUsername(userId);
+
+        AdminGetUserResult userResult = cognitoClient.adminGetUser(userRequest);
+
+        String firstName = null;
+
+        for (int i = 0; i < userResult.getUserAttributes().size(); i++) {
+            if(userResult.getUserAttributes().get(i).getName().equals("name")){
+                firstName = userResult.getUserAttributes().get(i).getValue();
+            }
+        }
+
+        return firstName;
+    }
+
+    public String getUserLastName(String userId) {
+        AdminGetUserRequest  userRequest  = new AdminGetUserRequest().withUserPoolId("ca-central-1_ZBpO9AVc6").withUsername(userId);
+
+        AdminGetUserResult userResult = cognitoClient.adminGetUser(userRequest);
+
+        String lastName = null;
+
+        for (int i = 0; i < userResult.getUserAttributes().size(); i++) {
+            System.out.println(userResult.getUserAttributes().get(i).getName());
+            if(userResult.getUserAttributes().get(i).getName().equals("family_name")){
+                System.out.println("test"+userResult.getUserAttributes().get(i).getName());
+                lastName = userResult.getUserAttributes().get(i).getValue();
+            }
+        }
+
+        return lastName;
+    }
+
+    public void changePassword(String authToken, String oldPassword, String newPassword) {
+
+        ChangePasswordRequest  passwordRequest = new ChangePasswordRequest()
+                .withAccessToken(authToken)
+                .withPreviousPassword(oldPassword)
+                .withProposedPassword(newPassword);
+
+        cognitoClient.changePassword(passwordRequest);
+
+
+    }
+
+    public void forgotPassword(String userName) {
+
+        ForgotPasswordRequest  forgotpasswordRequest = new ForgotPasswordRequest().withUsername(userName);
+
+        cognitoClient.forgotPassword(forgotpasswordRequest);
+
+
+    }
+
+    public  void setUserInfo(String userId,  String firstName, String lastName) {
+        AdminUpdateUserAttributesRequest  userRequest  = new AdminUpdateUserAttributesRequest().withUserPoolId("ca-central-1_ZBpO9AVc6").withUsername(userId);
+
+
+        Collection<AttributeType> attribute = new ArrayList<AttributeType>();
+        AttributeType attributeFirstName = new AttributeType();
+        attributeFirstName.setName("name");
+        attributeFirstName.setValue(firstName);
+        attribute.add(attributeFirstName);
+        AttributeType attributeLastName = new AttributeType();
+        attributeLastName.setName("family_name");
+        attributeLastName.setValue(lastName);
+        attribute.add(attributeLastName);
+        userRequest.withUserAttributes(attribute);
+        AdminUpdateUserAttributesResult userResult = cognitoClient.adminUpdateUserAttributes(userRequest);
+    }
+
+    public static void resetUserPassword(String userId, String userPoolId) {
+        AdminResetUserPasswordRequest request = new AdminResetUserPasswordRequest();
+        request.setUsername(userId);
+        request.setUserPoolId(userPoolId);
+        System.out.print(userId);
     }
 }
