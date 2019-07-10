f84bd95ce4f0d09584d922be76e9c64d6d8254d3
Physiix <yanis.m.1992@gmail.com>
2019-07-10 11:03:47 -0400

    Removes JDBC dependency

--- ProtogestService/src/main/java/com/protogest/service/ProtogestApplication.java
+++ ProtogestService/src/main/java/com/protogest/service/ProtogestApplication.java
@@ -1,10 +1,15 @@
 package com.protogest.service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages={
-         "com.protogest.service"})
+@SpringBootApplication(scanBasePackages = {
+        "com.protogest.service"},
+        exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ProtogestApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProtogestApplication.class, args);
